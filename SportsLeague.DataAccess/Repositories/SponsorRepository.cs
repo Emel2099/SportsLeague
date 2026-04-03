b6b64d0e3f76935eef00c8d93ac847155b18d7ee
Enmanuelle Perez <emanuel17229@gmail.com>
2026-04-02 20:10:13 -0500

    first advances

---
+++ SportsLeague.DataAccess/Repositories/SponsorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeague.DataAccess.Repositories
+{
+    internal class SponsorRepository
+    {
+    }
+}
---
+++ SportsLeague.Domain/Entities/TournamentSponsor.cs
@@ -0,0 +1,21 @@
+﻿
+namespace SportsLeague.Domain.Entities
+{
+    internal class TournamentSponsor : AuditBase
+    {
+        public int TournamentId { get; set; }
+
+        public int SponsorId { get; set; }
+
+        public decimal ContractAmount { get; set; } //Monto del contrato
+
+        public DateTime JoinedAt { get; set; } // Fecha de vinculación
+
+        // Navigation Properties
+
+        public Sponsor Sponsor { get; set; } = null!;
+
+        public Tournament Tournament { get; set; } = null!;
+
+    }
+}
